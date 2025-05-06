f9b42b5a11862f4229920b051d40720f59685205
LumberHat <lsb1412@hotmail.com>
2025-05-06 21:22:57 +0000

    test devops

--- pushall.sh
+++ pushall.sh
@@ -8,6 +8,4 @@ git config --global pull.rebase true
 
 git remote set-url origin https://$1@github.com/LumberHat/5667-game.git
 git branch -M main
-git pull origin staging
-git pull origin main --allow-unrelated-histories
 git push --force origin main 
