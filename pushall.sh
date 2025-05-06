e120ae98fe9d245f84abe6864728edfd72c1888c
LumberHat <lsb1412@hotmail.com>
2025-05-06 20:51:00 +0000

    aaaaah

--- pushall.sh
+++ pushall.sh
@@ -6,6 +6,6 @@ git config --global user.name "LumberHat"
 
 git remote set-url origin https://$1@github.com/LumberHat/5667-game.git
 git branch -M main
-# git config pull.rebase true
-# git pull origin main --allow-unrelated-histories
+git config pull.rebase true
+git pull origin main --allow-unrelated-histories
 git push origin main
