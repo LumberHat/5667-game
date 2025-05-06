3f70cb2c7aa804920aa7e06f6e5493a2971891bc
LumberHat <lsb1412@hotmail.com>
2025-05-06 20:08:50 +0000

    automated testing testing

--- pushall.sh
+++ pushall.sh
@@ -1,5 +1,4 @@
-$TOKEN=$1
 
-git remote set-url https://$TOKEN@github.com/LumberHat/5667-game.git
+git remote set-url https://$1@github.com/LumberHat/5667-game.git
 git pull origin main
 git push origin main
