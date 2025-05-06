408e81a02a1b7b363637136b8a45ddd9ea5f40ae
LumberHat <lsb1412@hotmail.com>
2025-05-06 20:12:32 +0000

    automated testing testing

--- pushall.sh
+++ pushall.sh
@@ -1,5 +1,5 @@
 
 git remote set-url origin https://$1@github.com/LumberHat/5667-game.git
 git config pull.rebase false
-git pull origin main
+git pull origin main --allow-unrelated-histories
 git push origin main
